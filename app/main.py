ad19b4e96d964bd9e5b32dc93ad2d9066790a435
CooperNiebuhr <cooper@cniebuhr.com>
2026-02-26 20:08:02 -0600

    add static UI shell

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.db import init_db
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.on_event("startup")
@@ -12,5 +17,11 @@ def _startup() -> None:
 
 
 @app.get("/", response_class=HTMLResponse)
-def home() -> str:
-    return "<html><body><h1>Health Tracker is up</h1></body></html>"
+def index(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "default_range": "30",
+        },
+    )
